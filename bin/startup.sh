90bbf433609e822415231509eade340cb83eca55
pnrao <prash.n.rao@gmail.com>
2015-04-07 13:13:52 +0200

    removed obsolete stuff from startup

--- bin/startup.sh
+++ bin/startup.sh
@@ -2,11 +2,8 @@
 
 xset r rate 250 30
 killall -HUP xbindkeys || xbindkeys
-(pkill -USR1 conky || conky -a top_left -b -d &)
 (start-pulseaudio-x11 --check || start-pulseaudio-x11 --start -D &)
 (pgrep xflux || xflux -l 49.26742 -g 7.0284 -k 3400 &)
-(pgrep connman-ui-gtk || connman-ui-gtk &)
-(pgrep launchy || launchy &)
-(pgrep volumeicon || volumeicon &)
-(xscreensaver-command -restart || xscreensaver &)
-(smartbg /home/prash/wallpapers &)
+(pgrep cmst || cmst -m &)
+(xscreensaver-command -restart || xscreensaver -no-splash &)
+(smartbg /usr/share/backgrounds &)
