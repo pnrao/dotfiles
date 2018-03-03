0c4de95fd63a57a2e278c8641443a8edd2fbafe4
pnrao <prash.n.rao@gmail.com>
2018-03-03 22:11:32 +0530

    flacize

--- bin/orphaned-files.sh
+++ bin/orphaned-files.sh
@@ -8,4 +8,4 @@ if [[ -n $@ ]] {
 
 find $dirs | sort > /tmp/all_files.txt
 pacman -Qlq | sed 's|/$||' | sort > /tmp/owned_files.txt
-comm -23 /tmp/all_files.txt /tmp/owned_files.txt
+comm -23 /tmp/all_files.txt /tmp/owned_files.txt > /tmp/orphan_files.txt
