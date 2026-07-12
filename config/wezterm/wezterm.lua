d4b30a838615b05dd7a9b2b7f3921209c91ee1b8
pnrao <prash.n.rao@gmail.com>
2026-07-12 12:10:03 +0530

    wezterm: disable SUPER-key bindings that conflict with WM shortcuts

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -26,8 +26,40 @@ config.default_prog = { '/bin/zsh' }
 
 config.keys = {
   { key = '_', mods = 'SHIFT|CTRL', action = wezterm.action.Nop },
-  { key = '-', mods = 'SUPER', action = wezterm.action.Nop },
   { key = '_', mods = 'SHIFT|CTRL', action = wezterm.action.SendKey({ key = "_", mods = "SHIFT|CTRL" }) },
+  { key = '-', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '0', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '1', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '2', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '3', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '4', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '5', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '6', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '7', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '8', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '9', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '=', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '[', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = ']', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '[', mods = 'SHIFT|SUPER', action = wezterm.action.Nop },
+  { key = ']', mods = 'SHIFT|SUPER', action = wezterm.action.Nop },
+  { key = '{', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '}', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = '{', mods = 'SHIFT|SUPER', action = wezterm.action.Nop },
+  { key = '}', mods = 'SHIFT|SUPER', action = wezterm.action.Nop },
+  { key = 'c', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = 'f', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = 'k', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = 'm', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = 'n', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = 'r', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = 't', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = 'v', mods = 'SUPER', action = wezterm.action.Nop },
+  { key = 'w', mods = 'SUPER', action = wezterm.action.Nop },
+}
+
+config.mouse_bindings = {
+  { event = { Drag = { streak = 1, button = 'Left' } }, mods = 'SUPER', action = wezterm.action.Nop },
 }
 
 -- Finally, return the configuration to wezterm:
